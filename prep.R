e60a80974fcbb652349f92af2698e8c3ea683527
minjay <mjfan@ucar.edu>
2015-07-29 09:20:10 -0600

    pca

---
+++ prep.R
@@ -0,0 +1,69 @@
+rm(list=ls())
+setwd("/Users/minjay/Downloads/PEF-data")
+library(MBA)
+library(fields)
+library(rgdal)
+library(ggplot2)
+library(spBayes)
+
+PEF.shp <- readOGR("shapefiles","PEF-bounds")
+
+# remove NAs
+PEF.plots <- na.omit(read.csv("PEF-plots.csv"))
+n.ref <- nrow(PEF.plots)
+n.dist <- 151
+
+PEF.LVIS <- read.csv("PEF-LVIS.csv")
+
+LVIS <- rbind(as.matrix(PEF.plots[, 8:158]), as.matrix(PEF.LVIS[, 3:153]))
+
+# PCA
+LVIS = scale(LVIS, center = TRUE)
+s <- svd(LVIS)
+var.exp <- cumsum(s$d^2/sum(s$d^2))
+V <- s$v
+T <- LVIS%*%V
+# need to do variable selection
+y1 <- PEF.plots$biomass.mg.ha
+y2 <- log(PEF.plots$stems.ha)
+y3 <- PEF.plots$basal.area.m2.ha
+r <- min(which(var.exp>=0.95))
+lm.data <- data.frame(cbind(y2, T[1:n.ref, 1:r]))
+lm.obj <- lm(y2~., data = lm.data)
+summary(lm.obj)
+lm.data <- data.frame(cbind(y, T[1:n.ref, 1:6]))
+lm.obj <- lm(y~., data = lm.data)
+summary(lm.obj)
+lm.obj <- lm(sqrt(y)~V2+V3+V4+V6+V7, data = lm.data)
+summary(lm.obj)
+
+ggplot(data = PEF.plots, aes(x = x.coords, y = y.coords, label = MU)) +
+  geom_point() + geom_text(aes(label = MU), hjust=0, vjust=0)
+
+PEF.plots.long <- reshape(PEF.plots, varying = colnames(PEF.plots)[8:158], v.names = "energy", 
+                          timevar = "height", times = 1:151, direction = "long")
+
+PEF.plots.long.sort <- PEF.plots.long[order(PEF.plots.long$id),]
+
+head(PEF.plots.long.sort)
+
+selected <- subset(PEF.plots.long.sort, PEF.plots.long.sort$MU %in% c("10", " 7B"))
+p <- ggplot(data = selected, aes(x = height, y = energy, group = id, colour = MU))
+p + geom_line()
+
+coords <- PEF.plots[,c("x.coords", "y.coords")]
+stems.ha <- PEF.plots$stems.ha
+
+##get bounding box of PEF
+b.box <- as.vector(t(bbox(PEF.shp)))
+
+surf <- mba.surf(cbind(coords, stems.ha), no.X=200, no.Y=200, extend=TRUE, sp=TRUE, b.box=b.box)$xyz.est
+
+##set the projection for the resulting sp object
+proj4string(surf) <- proj4string(PEF.shp)
+
+##make pretty figure
+surf <- as.image.SpatialGridDataFrame(surf[PEF.shp,])
+image.plot(surf, xaxs = "r", yaxs = "r", xlab="Easting (m)", ylab="Northing (m)", main="Trees per ha")
+plot(PEF.shp, add=TRUE)
+

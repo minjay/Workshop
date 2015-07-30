976df4bb926e799938ef05b91886633fd2574ddc
mlaforgia <marina.laforgia@gmail.com>
2015-07-30 15:33:50 -0600

    added variogram

--- prep.R
+++ prep.R
@@ -26,6 +26,7 @@ plot(pca.test)
 pc.score <- predict(pca.test)[1:451,1:2]
 PEF.data <- cbind(PEF.plots[,1:7],pc.score)
 PEF.predict <- cbind(PEF.LVIS[,1:2], predict(pca.test)[452:12865,1:2])
+<<<<<<< Updated upstream
 PEF.coords <- PEF.data[,3:4]
 Lid.coords <- as.matrix(PEF.LVIS[,1:2])
 
@@ -85,39 +86,52 @@ image.plot(w.mu.surf)
 
 
 
+=======
+  
+coords <- as.matrix(PEF.data[,3:4])
+>>>>>>> Stashed changes
 
-ggplot(data = PEF.plots, aes(x = x.coords, y = y.coords, label = MU)) +
-  geom_point() + geom_text(aes(label = MU), hjust=0, vjust=0)
+lm1 <- lm(biomass.mg.ha ~ PC1 + PC2, data = PEF.data)
+vg1 <- variog(coords = coords, data = resid(lm1))
+plot(vg1, title = )
 
-PEF.plots.long <- reshape(PEF.plots, varying = colnames(PEF.plots)[8:158], v.names = "energy", 
-                          timevar = "height", times = 1:151, direction = "long")
+D.max <- max(iDist(coords))
+starting <- list("phi" = 3/0.5*D.max, "sigma.sq" = 700, "tau.sq" = 500)
+tuning <- list("phi" = 0.04, "sigma.sq" = 0.04, "tau.sq" = 0.04)
 
-PEF.plots.long.sort <- PEF.plots.long[order(PEF.plots.long$id),]
+priors.1 <- list("beta.Norm"=list(rep(0,3), diag(1000,3)),
+                 "phi.Unif"=c(3/D.max, 3/1), "sigma.sq.IG"=c(2, 2),
+                 "tau.sq.IG"=c(2, 0.1))
 
-head(PEF.plots.long.sort)
+cov.model <- "exponential"
 
-selected <- subset(PEF.plots.long.sort, PEF.plots.long.sort$MU %in% c("10", " 7B"))
-p <- ggplot(data = selected, aes(x = height, y = energy, group = id, colour = MU))
-p + geom_line()
+n.samples = 2000
+n.report = 500
+verbose = TRUE
 
+<<<<<<< Updated upstream
 ##Plot of all curves
 p <- ggplot(data = PEF.plots.long.sort, aes(x = height, y = energy, group = id, colour = MU))
 p + geom_line()
 
 coords <- PEF.plots[,c("x.coords", "y.coords")]
 stems.ha <- PEF.plots$stems.ha
+=======
+m.1 <- spLM(basal.area.m2.ha ~ PC1 + PC2, data = PEF.data, coords=coords, starting=starting,
+            tuning=tuning, priors=priors.1, cov.model=cov.model,
+            n.samples=n.samples, verbose=verbose, n.report=n.report)
+plot(m.1)
+>>>>>>> Stashed changes
 
-##get bounding box of PEF
-b.box <- as.vector(t(bbox(PEF.shp)))
+burn.in <- 0.5*n.samples
+m.1 <- spRecover(m.1, start=burn.in, verbose=FALSE)
+coords <- cbind(PEF.LVIS[, 1], PEF.LVIS[, 2])
+spPredict(m.1, pred.covars=PEF.predict, pred.coords=Lid.coords, start=burn.in, thin=50, n.report=1)
 
-surf <- mba.surf(cbind(coords, stems.ha), no.X=200, no.Y=200, extend=TRUE, sp=TRUE, b.box=b.box)$xyz.est
+y.hat <- apply(m.1.pred$p.y.predictive.samples, 1, mean)
 
-##set the projection for the resulting sp object
-proj4string(surf) <- proj4string(PEF.shp)
+<<<<<<< Updated upstream
 
-##make pretty figure
-surf <- as.image.SpatialGridDataFrame(surf[PEF.shp,])
-image.plot(surf, xaxs = "r", yaxs = "r", xlab="Easting (m)", ylab="Northing (m)", main="Trees per ha")
-plot(PEF.shp, add=TRUE)
-
-
+=======
+quilt.plot(coords, y.hat)
+>>>>>>> Stashed changes
